7f0adb09f8ac8082ae411c0651152b0c30dd6011
bhavyab-042 <73235775+bhavyab-042@users.noreply.github.com>
2023-02-20 08:56:04 +0530

    Add files via upload

---
+++ build/precache-manifest.8fbdba5774a31156008fc644b899487b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03eaaa5fd55308324d0380396cba1311",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23628d98eef5edaafba7",
+    "url": "/static/css/main.3c2fef4c.chunk.css"
+  },
+  {
+    "revision": "872033dfc6e9f52510ae",
+    "url": "/static/js/2.2cc56b9f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2cc56b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23628d98eef5edaafba7",
+    "url": "/static/js/main.7095b7a7.chunk.js"
+  },
+  {
+    "revision": "2a5e9718c304aba39f84",
+    "url": "/static/js/runtime-main.0f5541c3.js"
+  },
+  {
+    "revision": "f4a8eb0ce27eae8b397292426901e034",
+    "url": "/static/media/logo.f4a8eb0c.png"
+  }
+]);
